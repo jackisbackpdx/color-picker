9bd99ef326b3558f2ff5e74af49bfa272318945f
Jackson Kelley <jackson.kelley.g@gmail.com>
2020-01-26 14:00:18 -0800

    made the initial buttons with React

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './index.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ColorPicker extends React.Component {
+  render() {
+    return (
+      <ul>
+        <button className='red'>Red</button>
+        <button className='blue'>Blue</button>
+        <button className='yellow'>Yellow</button>
+      </ul>
+    );
+  }
 }
 
-export default App;
+export default ColorPicker;
